1fe8d920beae79654cb1d314f0460f93a58a774b
William-H-Yang <118427211+William-H-Yang@users.noreply.github.com>
2022-11-23 23:39:44 -0500

    Delete js directory

--- js/script.js
+++
@@ -1,4 +0,0 @@
-
-
-    window.silex = window.silex || {}
-    window.silex.data = {"site":{"width":1200},"pages":[{"id":"page-home","displayName":"Home","link":{"linkType":"LinkTypePage","href":"#!page-home"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true,"opened":false}]}
